998dfb9d8a8a88e07aa7022f65595ff7ea97cd18
hyun <okmt0423@gmail.com>
2024-01-16 21:54:35 +0900

    Security 연습장
    CorsConfig 수정
    
    보완사항
    - 이미지 업로드시 업로드중 표시도 없고
    업로드중에 작성하기 클릭하면 이미지 데이터 없이 저장해 버림
    
    - 포스팅 리스트 페이징 만들어야됨

--- src/main/java/hyun/portfolio9/configures/CorsConfig.java
+++ src/main/java/hyun/portfolio9/configures/CorsConfig.java
@@ -2,19 +2,32 @@ package hyun.portfolio9.configures;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableScheduling
+@EnableTransactionManagement
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://15.152.189.106:3000")
+                .allowedMethods("OPTIONS, GET, POST, PUT, DELETE");
+    }
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("http://15.152.189.106:3000/**");
+        config.addAllowedOrigin("http://15.152.189.106:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
